e62044762ecde4f715925fb6507d53334bbaef6a
f10d0 <florian.dolzmann@mailbox.tu-dresden.de>
2024-04-21 17:37:04 +0200

    classify.sh aligned to csv order

--- util/classify.sh
+++ util/classify.sh
@@ -11,13 +11,13 @@ echo "total entries:"
 zcat $filename | wc -l
 
 echo "control record present:"
-zcat $filename | awk -F';' '{split($NF, a, ","); for (i in a) if (a[i] == "91.216.216.216" && length(a)==2) {print; break}}' | wc -l
+zcat $filename | awk -F';' '{split($4, a, ","); for (i in a) if (a[i] == "91.216.216.216" && length(a)==2) {print; break}}' | wc -l
 
 echo "recursive resolver:"
-zcat $filename | awk -F';' '$2 == $3 {split($NF, a, ","); matchFound = 0; ctrlPresent = 0; for (i in a) { if (a[i] == $3) matchFound = 1; if (a[i] == "91.216.216.216") ctrlPresent = 1;} if (length(a)==2 && matchFound && ctrlPresent) print}' | wc -l
+zcat $filename | awk -F';' '$2 == $3 {split($4, a, ","); matchFound = 0; ctrlPresent = 0; for (i in a) { if (a[i] == $3) matchFound = 1; if (a[i] == "91.216.216.216") ctrlPresent = 1;} if (length(a)==2 && matchFound && ctrlPresent) print}' | wc -l
 
 echo "recursive fwds:"
-zcat $filename | awk -F';' '$2 == $3 {split($NF, a, ","); matchFound = 0; ctrlPresent = 0; for (i in a) { if (a[i] == $3) matchFound = 1; if (a[i] == "91.216.216.216") ctrlPresent = 1;} if (length(a)==2 && !matchFound && ctrlPresent) print}' | wc -l
+zcat $filename | awk -F';' '$2 == $3 {split($4, a, ","); matchFound = 0; ctrlPresent = 0; for (i in a) { if (a[i] == $3) matchFound = 1; if (a[i] == "91.216.216.216") ctrlPresent = 1;} if (length(a)==2 && !matchFound && ctrlPresent) print}' | wc -l
 
 echo "transparent fwds:"
-zcat $filename | awk -F';' '$2 != $3 {split($NF, a, ","); for (i in a) if (a[i] == "91.216.216.216" && length(a)==2) {print; break}}' | wc -l
+zcat $filename | awk -F';' '$2 != $3 {split($4, a, ","); for (i in a) if (a[i] == "91.216.216.216" && length(a)==2) {print; break}}' | wc -l
